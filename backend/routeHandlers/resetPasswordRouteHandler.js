be9588528c8a239ad41320ffe62ca8ce30e07c6b
usmaan <usi.shah06@gmail.com>
2025-02-15 22:45:07 +0530

    Add reset password route handler and update auth routes

---
+++ backend/routeHandlers/resetPasswordRouteHandler.js
@@ -0,0 +1,1 @@
+const resetPasswordRouteHandler = async () => {};
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -4,7 +4,7 @@ import { verifyEmail } from "../routeHandlers/verifyEmailRouteHandler.js";
 import { logoutRouteHandler } from "../routeHandlers/logoutRouteHandler.js";
 import { loginRouteHandler } from "../routeHandlers/loginRouteHandler.js";
 import { forgetPasswordRoutehandler } from "../routeHandlers/forgotPasswordRouteHandler.js";
-
+import { resetPasswordRouteHandler } from "../routeHandlers/resetPasswordRouteHandler.js";
 const router = express.Router();
 
 router.post("/signup", signupRouteHandler);
@@ -12,5 +12,6 @@ router.post("/verify-email", verifyEmail);
 router.post("/logout", logoutRouteHandler);
 router.post("/login", loginRouteHandler);
 router.post("/reset-password", forgetPasswordRoutehandler);
+router.post("/reset-password/:token", resetPasswordRouteHandler);
 
 export default router;
