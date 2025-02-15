cf6207f8f71b11100a9631697f70c077ced9cc88
usmaan <usi.shah06@gmail.com>
2025-02-15 23:25:30 +0530

    Remove duplicate router initialization in auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,12 +1,11 @@
 import express from "express";
+const router = express.Router();
 import { signupRouteHandler } from "../routeHandlers/signupRouteHandler.js";
 import { verifyEmail } from "../routeHandlers/verifyEmailRouteHandler.js";
 import { logoutRouteHandler } from "../routeHandlers/logoutRouteHandler.js";
 import { loginRouteHandler } from "../routeHandlers/loginRouteHandler.js";
 import { forgetPasswordRoutehandler } from "../routeHandlers/forgotPasswordRouteHandler.js";
 import { resetPasswordRouteHandler } from "../routeHandlers/resetPasswordRouteHandler.js";
-
-const router = express.Router();
 
 router.post("/signup", signupRouteHandler);
 router.post("/verify-email", verifyEmail);
