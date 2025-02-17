4b0b25601b66c955b169c3cb69a095f8913cae6d
usmaan <usi.shah06@gmail.com>
2025-02-17 21:18:55 +0530

    Add routing for LoginPage and SignupPage in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 import SignupPage from "./pages/SignupPage";
+import LoginPage from "./pages/LoginPage";
 
 const App = () => {
   return (
     <div>
       <SignupPage />
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/" element={<SignupPage />} />
+      </Routes>
     </div>
   );
 };
