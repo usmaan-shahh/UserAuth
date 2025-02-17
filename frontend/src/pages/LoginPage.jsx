fb9fd6a7537d22dd06c173936119ecad90911a1e
usmaan <usi.shah06@gmail.com>
2025-02-17 22:41:45 +0530

    Bold headings in LoginPage and SignupPage components for improved visibility

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -8,7 +8,9 @@ const LoginPage = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-        <h2 className="text-2xl font-semibold text-center mb-4">Login Below</h2>
+        <h2 className="text-2xl font-semibold text-center mb-4">
+          <b>Login Below</b>
+        </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
             <label
--- frontend/src/pages/SignupPage.jsx
+++ frontend/src/pages/SignupPage.jsx
@@ -9,7 +9,7 @@ const SignupPage = () => {
     <div className="flex items-center justify-center min-h-screen bg-gray-100">
       <div className="bg-white p-6 rounded-lg shadow-lg w-96">
         <h2 className="text-2xl font-semibold text-center mb-4">
-          Register Below
+          <b>Register Below</b>
         </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
