049a3c847b4583619fbf36a22ef2ebf78cda5d7e
usmaan <usi.shah06@gmail.com>
2025-02-17 21:37:36 +0530

    Refactor routing in App component to use RouterProvider and createBrowserRouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,8 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import SignupPage from "./pages/SignupPage";
-import LoginPage from "./pages/LoginPage";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  return (
-    <div>
-      <SignupPage />
-      <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/" element={<SignupPage />} />
-      </Routes>
-    </div>
-  );
+  return <div>{Outlet}</div>;
 };
 
 export default App;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,26 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { RouterProvider } from "react-router-dom";
+import React from "react";
+import LoginPage from "./pages/LoginPage.jsx";
+import SignupPage from "./pages/SignupPage.jsx";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <RouterProvider router={router} />
 );
