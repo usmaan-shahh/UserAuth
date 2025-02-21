3942234541b51c1cd2866fef68cafa022844314a
usmaan <usi.shah06@gmail.com>
2025-02-21 23:05:23 +0530

    Enhance express.json() middleware comment with additional context

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ import cors from "cors";
 const PORT = process.env.PORT;
 
 // Middleware to parse JSON request body
-app.use(express.json()); // express.json() converts incoming JSON data into a JavaScript object.
+app.use(express.json()); // express.json() converts incoming JSON data into a JavaScript object. It expects the incoming request to have a JSON body.
 
 app.use(cookieParser());
 
