746e3701fe195438e2c0cbf81ce638c52e380ecd
usmaan-shahh <usm017214@iust.ac.in>
2025-12-12 20:38:48 +0530

    Remove unused route handlers for password reset and email verification to streamline the authentication process and improve code maintainability.

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,20 +1,12 @@
 import express from "express";
 const router = express.Router();
-import { signupRouteHandler } from "../routeHandlers/signupRouteHandler.js";
-import { verifyEmail } from "../routeHandlers/verifyEmailRouteHandler.js";
-import { logoutRouteHandler } from "../routeHandlers/logoutRouteHandler.js";
-import { loginRouteHandler } from "../routeHandlers/loginRouteHandler.js";
-import { forgetPasswordRoutehandler } from "../routeHandlers/forgotPasswordRouteHandler.js";
-import { resetPasswordRouteHandler } from "../routeHandlers/resetPasswordRouteHandler.js";
-import { verifyToken } from "../middleware/verifyToken.js";
-import checkAuthRouteHandler from "../routeHandlers/checkAuthRouteHandler.js";
+import loginRouteHandler from "../routeHandlers/loginRouteHandler.js";
 
-router.post("/signup", signupRouteHandler);
+router.get("/refresh", verifyToken, checkAuthRouteHandler);
+router.post("/signup", loginLimiter, signupRouteHandler);
 router.post("/verify-email", verifyEmail);
 router.post("/logout", logoutRouteHandler);
 router.post("/login", loginRouteHandler);
-router.post("/reset-password", forgetPasswordRoutehandler);
-router.post("/reset-password/:token", resetPasswordRouteHandler);
-router.get("/check-auth", verifyToken, checkAuthRouteHandler);
+
 
 export default router;
