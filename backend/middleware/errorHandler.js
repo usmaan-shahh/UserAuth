31328cc533f23a861892b55e1823f08994134216
usmaan-shahh <usm017214@iust.ac.in>
2025-12-12 23:31:53 +0530

    Remove console stack trace logging from error handler to streamline error output and improve logging clarity.

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -4,7 +4,6 @@ const errorHandler = (error, req, res, __) => {
 
     logEvents(`${error.name}: ${error.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
 
-    console.log(error.stack)
 
     const status = res.statusCode ? res.statusCode : 500 // server error 
 
