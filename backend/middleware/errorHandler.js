6c30f3f4d9dfd609366e5cc4174af04c8c5c0736
usmaan-shahh <usm017214@iust.ac.in>
2025-12-12 23:44:55 +0530

    Remove unnecessary blank line in error handler for improved code clarity and consistency.

--- backend/middleware/errorHandler.js
+++ backend/middleware/errorHandler.js
@@ -4,7 +4,6 @@ const errorHandler = (error, req, res, __) => {
 
     logEvents(`${error.name}: ${error.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, 'errLog.log')
 
-
     const status = res.statusCode ? res.statusCode : 500 // server error 
 
     res.status(status)
