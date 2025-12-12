967efd36b40e54cdacb311956b8915e4aedc1d75
usmaan-shahh <usm017214@iust.ac.in>
2025-12-12 20:49:40 +0530

    Add authentication controller and update user model for login, refresh, and logout functionality

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,19 +1,22 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose'
 
-const userSchema = new mongoose.Schema(
-  {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    name: { type: String, required: true },
-    lastLogin: { type: Date, default: Date.now },
-    isVerified: { type: Boolean, default: false },
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    roles: {
+        type: [String],
+        default: ["Employee"]
+    },
+    active: {
+        type: Boolean,
+        default: true
+    }
+})
 
-    resetPasswordToken: String,
-    resetPasswordExpiresAt: Date,
-    verificationToken: String,
-    verificationTokenExpiresAt: Date,
-  },
-  { timestamps: true }
-);
-
-export const User = mongoose.model("User", userSchema);
+export const User = mongoose.model('User', userSchema)
